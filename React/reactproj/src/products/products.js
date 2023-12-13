531b2c83159859b0b5134029a32564699464a83f
Jeremy <barycki@student.agh.edu.pl>
2023-12-13 17:48:46 +0100

    Auto stash before merge of "React" and "origin/React"
    4 zadanie done

--- React/reactproj/src/products/products.js
+++ React/reactproj/src/products/products.js
@@ -2,10 +2,13 @@ import React, {useState, useEffect} from "react";
 import ProductsList from "./ProductList";
 import ReactDOM from "react-dom/client";
 import './products.css'
-// eslint-disable-next-line react-hooks/rules-of-hooks
-let data, setData;
-let productsdata, setProductsData;
-function OpenData() {
+import product from "./Product";
+
+
+function Products() {
+    const [data, setData] = useState([]);
+    const [productsdata, setProductsData] = useState([]);
+
 
     useEffect(() => {
         fetch("https://dummyjson.com/products")
@@ -17,35 +20,53 @@ function OpenData() {
             })
             .catch((error) => console.log(error));
     }, []);
+
+    const sortItems = (type) => {
+        let dataToSort = [...productsdata]
+        switch (type) {
+            case "dsc":
+                setProductsData(sortProducts(dataToSort))
+                break;
+            case "asc":
+                setProductsData(sortProducts(dataToSort).reverse())
+                break;
+            case "none":
+                setProductsData([...productsdata.sort((a, b) => a.id - b.id)])
+                break;
+        }
+        console.log(productsdata)
+    }
+    const sortProducts = (dataToSort) => {
+        //Sortuje po nazwie
+        if (dataToSort) {
+            dataToSort.sort((a, b) => {
+                const strA = replaceNotLetters(a.title);
+                const strB = replaceNotLetters(b.title);
+                return strA.localeCompare(strB);
+            });
+            return dataToSort;
+        }
+    }
+    const searchItems = (e) => {
+        let searchPhrase = e.target.value
+        console.log(searchPhrase)
+        setProductsData(data.filter((product) => product.title.toLowerCase().includes(searchPhrase)))
+    }
+    return (<><div className="button-conteiner"><div className="buttons"><div className="sort-buttons">
+        <div className={"Dsc"} onClick={() => sortItems('dsc')}>DSC</div>
+        <div className={"Asc"} onClick={() => sortItems('asc')}>ASC</div>
+        <div className={"Default"} onClick={() => sortItems('none')}>DEFAULT</div></div>
+        <input type="text" placeholder={"search"} onChange={(e)=>searchItems(e)}/></div>
+    </div>
+        <ProductsList products={productsdata}/></>)
 }
-function Products(){
-    [data, setData] = useState([]);
-    [productsdata, setProductsData] = useState([]);
-    OpenData();
-    return(<div><Dsc onClick={()=>searchItems(0)}/><ProductsList products={productsdata}/></div>)
-}export default Products
-function Dsc(){
-    return(<div className={"dsc"} onClick={onclick}>DSC</div>)
-}
-const searchItems=(type)=>{
-   setProductsData(sortProducts(productsdata))
-    console.log(productsdata)
-}
+
+export default Products
+
+
 function replaceNotLetters(text) {
     //Funkcja pomocnicza do sortowania która usuwa znaki niebędące literami
     text = text.replace(/3D /g, "");
     text = text.toLowerCase();
     return text.replace(/[^a-z]/g, "");
-}
-function sortProducts(dataToSort) {
-    //Sortuje po nazwie
-    const newdataToSort=dataToSort
-    if (newdataToSort) {
-        newdataToSort.sort((a, b) => {
-            const strA = replaceNotLetters(a.title);
-            const strB = replaceNotLetters(b.title);
-            return strA.localeCompare(strB);
-        });
-        return newdataToSort;
-    }
 }
